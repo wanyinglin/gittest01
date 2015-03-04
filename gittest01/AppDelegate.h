ad59df6304d8b19341972bba29d1d9c4c13d26f2
WanYing <wanying@LinWanYingdeMac-mini.local>
2015-03-04 16:52:53 +0800

    version 0.9

---
+++ gittest01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gittest01
+//
+//  Created by WanYing on 2015/3/4.
+//  Copyright (c) 2015年 WanYing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gittest01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittest01
+//
+//  Created by WanYing on 2015/3/4.
+//  Copyright (c) 2015年 WanYing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
